65c747a76310b46f1555d21ecbcf68d4dfa8cdbb
GagandeepLubana <gagandeeplubana1221@gmail.com>
2024-08-02 23:43:39 -0400

    added mongo schmea

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors());
 
-app.use("/", (req, res) => {})
+app.use("/outfit", (req, res) => {})
 
 const PORT = process.env.PORT || 3001;
 
---
+++ server/src/schema/Outfit.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const outfitSchema = mongoose.Schema({
+    title: String,
+    creater: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    }
+})
+
+const outfitModel = mongoose.model("Outfit", outfitSchema);
+
+export default outfitModel
