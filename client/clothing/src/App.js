46472927de3ff600e629c8968cbc4e8c4b9cfe0d
tiwoah <corextyy@gmail.com>
2024-08-03 11:51:21 -0400

    style: landing page and try on page

--- client/clothing/src/App.js
+++ client/clothing/src/App.js
@@ -1,15 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Sidebar from "./components/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Main from "./pages/TryOn";
 
 function App() {
   return (
-    <div className="bg-primary">
+    <div className="bg-primary text-textPrimary">
       <BrowserRouter>
         <Routes>
           <Route index element={<Landing />} />
+          <Route path="/tryon" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>
